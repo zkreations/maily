a6868c0e44385e9ec3cf66f2aa30b8d95b626410
Daniel Abel <daniel@zkreations.com>
2023-02-03 05:49:28 -0500

    build: hack js de blogger

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,17 @@ const dir = {
   xml: './themes/*/*.xml'
 }
 
+const blogger = {
+  jsHack: `&lt;textarea id=&quot;bjs&quot; readonly hidden&gt;</body>&lt;/textarea&gt;
+  <script>document.getElementById('bjs').remove()</script>&lt;/body&gt;`,
+  tags: /<(b:*[^/>]*)>\s*<\/(b:*[^>]*)>/g,
+}
+
 // Plugins
 const rename = require("gulp-rename")
 const pug = require('gulp-pug')
+
+const replace = require('gulp-replace')
 
 const build = () => {
   return src( [dir.themes, `!${dir.allFolders}_*`], { base: "." } )
@@ -18,6 +26,8 @@ const build = () => {
       pretty: true,
       basedir: "./"
     }))
+    .pipe(replace(blogger.tags, "<$1/>"))
+    .pipe(replace('</body>', blogger.jsHack))
     .pipe( rename( { extname: '.xml' } ) )
     .pipe( dest( './' ) )
 }
